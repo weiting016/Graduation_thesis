8215a8d78f72b03b2ba3dbaa9cc070f95d7359a4
weiting016 <qwttt1213@gmail.com>
2025-01-25 15:32:40 +0100

    modify drift enn with add actions and states

--- main/dynamic_env.py
+++ main/dynamic_env.py
@@ -37,8 +37,9 @@ class TaskEnv_drift():
           'naar andere kamer/ruimte gestuurd',
           'met kracht tegen- of vastgehouden',
           'afzondering (deur op slot)'] #action
-        self.states = ['va','sib','pp','po','Tau'] 
         
+        self.states = ['va','sib','pp','po','Tau'] 
+        self.action_penalty_d = None #record added action with its penalty in diction form
         frequencies = pd.read_csv("frequencies.csv", index_col=0)
         for label in frequencies:
             for action in self.motions:
@@ -50,10 +51,11 @@ class TaskEnv_drift():
         self.timeout_reward = timeout_reward
         self.goal_reward = goal_reward
         self.time_reward_multiplicator = time_reward_multiplicator
-        self.seed()
+        self.seed(2)
         self.observation_space = frequencies
         self.action_space = Discrete(len(self.motions))
-        self.positions = np.random.choice(['va','pp','po','sib'], size=1)
+        self.choice_positions =['va','pp','po','sib']
+        self.positions = np.random.choice(self.choice_positions, size=1)
         self.episode_actions = []
 
     def step(self, action: int, info = False):
@@ -65,7 +67,7 @@ class TaskEnv_drift():
         if self.flag == False: #without drift
             action_penalty = self.get_action_reward(motion)
         else: #add drift, use extended action_reward function
-            action_penalty = self.get_action_reward_drift(motion)
+            action_penalty = self.get_action_reward(motion,self.action_penalty_d)
 
         self.positions = new_position
         self.episode_actions.append((action, current_position, new_position))
@@ -95,7 +97,7 @@ class TaskEnv_drift():
             Tuple[int, int]: The initial position of the agent.
         """
         self.timer = 0
-        self.positions = np.random.choice(['va','pp','po','sib'])
+        self.positions = np.random.choice(self.choice_positions)#(['va','pp','po','sib'])
         self.episode_actions = []
         return self.positions
                                           
@@ -134,78 +136,71 @@ class TaskEnv_drift():
 
         return np.random.choice(states, size=1, p=p)
     
-    def get_action_reward(self, action):
+    def get_action_reward(self, action,add_penalty_dict = None):
         penalty = 0
         if action == 'met kracht tegen- of vastgehouden' or action == 'afzondering (deur op slot)':
             penalty = -2
         elif action == 'client afgeleid' or action == 'contact beeindigd/weggegaan' or action == 'naar andere kamer/ruimte gestuurd':
             penalty = -1
+        if add_penalty_dict != None:
+            penalty.get(action)
+
         return penalty
 
     
     def set_flag(self):
         self.flag = True
+    
         return 
     
-    def drift(self):
+    
+    def drift(self,add_actions=0,add_states=0):
         """
         change of the env: the observation space: add actions and states
         assign new transition probability
         """
         if self.flag == True:
+#state action总数不变，只是部分path关闭
+            print("drift happen")
+            #self.observation_space  = change_frequencies(self.motions, self.states)
+    
+            if add_actions != 0:
+                syn_actions = syn_state_action('a',add_actions)#['a1','a2','a3']
+                print(syn_actions)
 
-            syn_actions = ['a1','a2','a3']
-            syn_states = ['s1','s2','s3']
-            self.motions.extend(syn_actions)
-            self.states.extend(syn_states)
-            self.observation_space  = added_frequencies(self.motions, self.states)
+                self.motions.extend(syn_actions)
+                self.action_penalty_d = action_penalty_dict(syn_actions)
+            if add_states != 0:
+                syn_states = syn_state_action('s',add_states)
+                self.states.extend(syn_states)
+                self.choice_positions.extend(syn_states)
+                change_states_severity(self,syn_states)
+
+            self.observation_space  = change_frequencies(self.motions, self.states)
             self.action_space = Discrete(len(self.motions))
-            self.severity = {'va': 0.0, 
+
+            """self.severity = {'va': 0.0, 
                             'po': -1.0,
                             'sib': -3.0,
                             'pp' : -4.0,
                             'Tau': 1.0,
                             's1': 0.0,
                             's2': 1.0,
-                            's3': 1}  #gained reward when reach certain state
-        
+                            's3': 1}  #gained reward when reach certain state"""   
         return
-    
-    def reset_drift(self) -> str:
-        """Resets the environment. The agent will be transferred to a random location on the map. The goal stays the same and the timer is set to 0.
 
-        Returns:
-            Tuple[int, int]: The initial position of the agent.
-        """
-        self.timer = 0
-        self.positions = np.random.choice(['va','pp','po','sib','s1','s2','s3'])
-        self.episode_actions = []
-        return self.positions
 
-    def get_action_reward_drift(self, action):
-        penalty = 0
-        if action == 'met kracht tegen- of vastgehouden' or action == 'afzondering (deur op slot)':
-            penalty = -2
-        elif action == 'client afgeleid' or action == 'contact beeindigd/weggegaan' or action == 'naar andere kamer/ruimte gestuurd':
-            penalty = -1
 
-        #add new state with different reward 
-        elif action =='a1' or action == 'a2':
-            penalty = 1
-        elif action == 'a3':
-            penalty = -3
-        return penalty
-
-def added_frequencies(actions,states):
+def change_frequencies(actions,states):
+   # print('execute change frequencies')
     new_freq = pd.DataFrame(index = actions)
     action_num = len(actions)
     for s in states:
-        new_freq[s] = list(map(create_transition, repeat(states,action_num)))
-    #print(new_freq.shape)
+        #new_freq[s] = list(map(create_transition, repeat(states,action_num)))
+        new_freq[s] = list(map(drift_transition, repeat(states,action_num)))
+   # print(new_freq.shape)
     return new_freq
     
-#def create_transition_closepath(states):
-    #"""disable certain path to certain state"""
 
 def create_transition(states): 
     """
@@ -214,22 +209,59 @@ def create_transition(states):
     like Q-learning alpha
     type of drift
     """
-
+    #print("excute create transitions ")
     np.random.seed(2) #set the random seed make result reproducable
     possible_path = len(states) #possible transitions number
     path_num = random.randint(1, possible_path+1)  #assigned real path
     trans_prob = np.random.dirichlet(np.ones(path_num), size=1)[0] #assign transition probability 可能长度不一样
 
     if path_num < possible_path:
-        #np.concatenate((trans_prob),[0]*(possible_path- path_num))
+        np.concatenate((trans_prob),[0]*(possible_path- path_num))
         trans_prob = list(trans_prob)
         trans_prob.extend([0]*(possible_path- path_num))
         
     np.random.shuffle(trans_prob) #possible closed pass can appear random posation
-    #keys = states
-    #valuess = trans_prob
     trans_dict = {s: t for s, t in zip(states, trans_prob)}
    # print(trans_dict)
     return trans_dict
 
 
+
+def drift_transition(states):
+    """
+    the drift happen in transition probability
+    if close = True prob can become 0 (path close),otherwise just change transition probability
+    do not influence the size state and action space
+    """
+    #print('excute drift transition')
+    np.random.seed(2) #set the random seed make result reproducabl
+    possible_path = len(states) #possible transitions number
+    #if self.close == True:
+    path_num = random.randint(1, possible_path)  #assigned real path,less than orignal pathes
+    trans_prob = np.random.dirichlet(np.ones(path_num), size=1)[0] #assign transition probability 可能长度不一样
+   # print(type(trans_prob))
+   # print("trans_prob:", trans_prob)
+    if path_num < possible_path:
+        np.concatenate(((trans_prob),[0]*(possible_path- path_num)))
+        trans_prob = list(trans_prob)
+        trans_prob.extend([0]*(possible_path- path_num))
+        
+    np.random.shuffle(trans_prob) #possible closed pass can appear random posation
+    trans_dict = {s: t for s, t in zip(states, trans_prob)}
+   # print(trans_dict)
+    return trans_dict
+
+def action_penalty_dict(add_action):
+    numadd = len(add_action)
+    penalty = np.random.randint(-5.0,0,numadd)
+    return {s: t for s, t in zip(add_action, penalty)}
+    
+def syn_state_action(prefix,n):
+    return [f"{prefix}{i}" for i in range(n)]
+
+def change_states_severity(self,add_states):
+    add_num = len(add_states)
+    add_severity = np.random.randint(-5.0,5.0,add_num) #define severity with in range -5.0 - 5.0
+    severity_dict = {s: t for s, t in zip(add_states, add_severity)}
+    self.severity.update(**severity_dict)
+    return
